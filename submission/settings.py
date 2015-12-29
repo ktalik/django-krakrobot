b5711271f910065cd8613f6ff7a6b021628ec59a
Konrad Talik <konradtalik@gmail.com>
2015-12-29 02:03:00 +0100

    STATIC_ROOT

--- submission/settings.py
+++ submission/settings.py
@@ -111,7 +111,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-#STATIC_ROOT = os.path.join(WSGI_DIR, 'static')
+STATIC_ROOT = os.path.join(WSGI_DIR, 'static')
 
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
