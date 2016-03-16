74f834c55adcf4112c07a3660c7b6fe9230c9f23
Maciej Ziarkowski <ziarrek@gmail.com>
2016-03-16 06:03:32 +0100

    Turn on meta context processor in settings

--- submission/settings.py
+++ submission/settings.py
@@ -82,7 +82,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                #'context_processors.meta_data',
+                'submission.context_processors.meta_data',
             ],
         },
     },
